e96d29ab459127f126959b7428ed06cbe48c971f
Gaurav Singh <gaurav2k50@gmail.com>
2024-12-17 23:05:46 +0530

    Remove frontend folder from repository

--- frontend/src/app/components/Settings/Setting.tsx
+++
@@ -1,7 +0,0 @@
-export default function Setting() {
-  return (
-    <>
-      <h1>This is Setting</h1>
-    </>
-  );
-}
--- frontend/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-// import Image from "next/image";
-
-// import styles from "./page.module.css";
-
-export default function Home() {
-  return <>{/* <h1>This is main page</h1> */}</>;
-}
